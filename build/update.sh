302ad077e40b4c969f50325ab9bf7e3b37999da8
vkill <vkill.net@gmail.com>
2013-06-05 09:17:46 +0800

    update rivets.js to 0.5.7

--- build/update.sh
+++ build/update.sh
@@ -1,3 +1,3 @@
-TAG=v0.5.4
+TAG=v0.5.7
 curl -s -o dist/rivets.js https://raw.github.com/mikeric/rivets/$TAG/dist/rivets.js
 curl -s -o dist/rivets.min.js https://raw.github.com/mikeric/rivets/$TAG/dist/rivets.min.js
